f98835bab8dcc0829ead024e3e7f62c467d1223d
Alexandru_Caracas <loredal204@gmail.com>
2026-03-09 14:27:20 +0200

    feat: add most core functionality
    Most features are now implemented such as time-picker, temperature display, details-display, conditions, and city.
    Remaining work includes minor details and performance optimization.

--- script.js
+++ script.js
@@ -1,9 +1,160 @@
-document.addEventListener("DOMContentLoaded", () => {
-    flatpickr("#date-input", {
-        enableTime: true,      // show time picker
-        noCalendar: false,     // show calendar
-        dateFormat: "m/d D | H:00", // only hour, no minutes
-        time_24hr: true,       // 24h format
-        minuteIncrement: 60    // step = 1 hour
+const today = new Date();
+
+for (let i = 1; i <= 5; i++) {
+    const date = new Date();
+    date.setDate(today.getDate() + i - 1);
+
+    const dayName = date.toLocaleDateString("en-US", { weekday: "long" });
+    const dayNumber = date.getDate();
+
+    const column = document.getElementById("day" + i);
+
+    column.querySelector(".day-name").textContent = dayName;
+
+    if (i === 1) {
+        column.querySelector(".date").textContent = "Today";
+    } else if (i === 2) {
+        column.querySelector(".date").textContent = "Tomorrow";
+    }
+     else {
+        column.querySelector(".date").textContent = `${dayNumber}/${date.getMonth() + 1}`;
+    }
+}
+
+const input = document.getElementById("time-input");
+const dropdown = document.getElementById("time-dropdown");
+
+for(let h=0; h<24; h++){
+
+    const hour = h.toString().padStart(2,'0') + ":00";
+
+    const option = document.createElement("div");
+    option.className = "time-option";
+    option.textContent = hour;
+
+    option.addEventListener("click", () => {
+        input.value = hour;
+        dropdown.style.display = "none";
+    });
+
+    dropdown.appendChild(option);
+}
+
+dropdown.addEventListener("click", () =>{
+    updateWeather();
+})
+
+input.addEventListener("click", () => {
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+});
+
+
+// click outside
+document.addEventListener("click", (e)=>{
+    if(!e.target.closest(".time-picker")){
+        dropdown.style.display = "none";
+    }
+});
+
+window.addEventListener('pywebviewready', async function(){
+    console.log("PyWebView ready!");
+    updateWeather();
+    
+    const cities = await window.pywebview.api.get_available_cities();
+    
+    const datalist = document.getElementById('cities');
+    cities.forEach(city => {
+        const option = document.createElement('option');
+        option.value = city;
+        datalist.appendChild(option);
     });
 });
+
+window.addEventListener('pywebviewready', () => {
+    const searchIcon = document.querySelector('.search-icon-abs');
+    const cityInput = document.getElementById('city-input');
+
+
+    searchIcon.addEventListener('click', async () => {
+        updateWeather();
+    });
+
+    cityInput.addEventListener('keydown', async (event) => {
+        if (event.key === "Enter")  {
+            updateWeather();
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const dayLinks = document.querySelectorAll('.day-link');
+    const timeInput = document.getElementById('time-input');
+
+    dayLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); 
+
+            document.querySelectorAll('.day-column').forEach(column => {
+                column.classList.remove('active');
+            });
+
+            const dayColumn = link.querySelector('.day-column');
+            if (dayColumn) {
+                dayColumn.classList.add('active');
+                timeInput.value = "";
+                updateWeather();
+            }
+        });
+    });
+});
+
+
+async function updateWeather() {
+        const cityInput = document.getElementById('city-input');
+        const timeInput = document.getElementById('time-input');  
+        const active = document.querySelector('.active').id;
+        
+        const today = new Date();
+        let date = new Date();
+        let time = "now";
+        let city = "Timisoara";
+
+        for (let i = 0; i <5; i++){
+            if(active === 'day' + i){
+                date.setDate(today.getDate() + i)
+            }
+        }
+
+        date = date.toISOString().split('T')[0];
+
+        if(timeInput.value !== ""){
+            time = timeInput.value;
+        }
+
+        if(cityInput.value !== ""){
+            city = cityInput.value;
+        }
+
+        const weatherInfo = await window.pywebview.api.get_weather_info(city, time, date);
+
+        const temperatureElement = document.getElementById('temperature-now');
+        temperatureElement.textContent = `${weatherInfo.temperature}°C`; 
+
+        const cityNameElement = document.getElementById('city-name');
+        city = city.charAt(0).toUpperCase() + city.slice(1);
+        cityNameElement.textContent = city;
+
+        timeInput.value = weatherInfo.time;
+        
+        const conditionElement = document.getElementById('condition');
+        conditionElement.textContent = `${weatherInfo.condition.name}`;
+
+        const mainEmojiElement = document.getElementById('main-emoji');
+        mainEmojiElement.textContent = `${weatherInfo.condition.emoji}`;
+
+        const windElement = document.getElementById('wind');
+        windElement.textContent = `Wind: ${weatherInfo.wind} km/h`;
+
+        const feelingElement = document.getElementById('feeling');
+        feelingElement.textContent = `Feels like: ${weatherInfo.feeling} °C`;
+}
--- weather.py
+++ weather.py
@@ -1,18 +1,130 @@
 #!/usr/bin/env python3
 
 import webview
-import weatherNotification
+import json
+import requests
+from datetime import datetime
+
+WMO = {
+    0: {"name": "Clear sky", "emoji": "☀️"},
+    1: {"name": "Mainly clear", "emoji": "🌤️"},
+    2: {"name": "Partly cloudy", "emoji": "⛅"},
+    3: {"name": "Overcast", "emoji": "☁️"},
+    45: {"name": "Fog", "emoji": "🌫️"},
+    48: {"name": "Depositing rime fog", "emoji": "🌫️"},
+    51: {"name": "Light drizzle", "emoji": "🌦️"},
+    53: {"name": "Moderate drizzle", "emoji": "🌦️"},
+    55: {"name": "Dense drizzle", "emoji": "🌧️"},
+    56: {"name": "Light freezing drizzle", "emoji": "🌧️"},
+    57: {"name": "Dense freezing drizzle", "emoji": "🌧️"},
+    61: {"name": "Slight rain", "emoji": "🌧️"},
+    63: {"name": "Moderate rain", "emoji": "🌧️"},
+    65: {"name": "Heavy rain", "emoji": "🌧️"},
+    66: {"name": "Light freezing rain", "emoji": "🌧️"},
+    67: {"name": "Heavy freezing rain", "emoji": "🌧️"},
+    71: {"name": "Slight snow", "emoji": "🌨️"},
+    73: {"name": "Moderate snow", "emoji": "🌨️"},
+    75: {"name": "Heavy snow", "emoji": "❄️"},
+    77: {"name": "Snow grains", "emoji": "❄️"},
+    80: {"name": "Slight rain showers", "emoji": "🌦️"},
+    81: {"name": "Moderate rain showers", "emoji": "🌦️"},
+    82: {"name": "Violent rain showers", "emoji": "⛈️"},
+    85: {"name": "Slight snow showers", "emoji": "🌨️"},
+    86: {"name": "Heavy snow showers", "emoji": "❄️"},
+    95: {"name": "Thunderstorm", "emoji": "⛈️"},
+    96: {"name": "Thunderstorm with slight hail", "emoji": "⛈️"},
+    99: {"name": "Thunderstorm with heavy hail", "emoji": "⛈️"},
+}
 
 class Api:
-    def get_weather(self, city, day, time):
-        return weatherNotification.get_weather()
+    def get_weather_info(self, city, time, date):
+        geo_url = "https://geocoding-api.open-meteo.com/v1/search"
+        geo_params = {"name": city, "count": 1}
+        geo_res = requests.get(geo_url, params=geo_params).json()
+        
+        if "results" in geo_res:
+            lat = geo_res["results"][0]["latitude"]
+            lon = geo_res["results"][0]["longitude"]
+
+        weather_url = "https://api.open-meteo.com/v1/forecast"
+        weather_params = {
+            "latitude": lat,
+            "longitude": lon,
+            "hourly": "temperature_2m,weathercode,apparent_temperature,wind_speed_10m",
+        }
+
+        target_date = datetime.strptime(date, "%Y-%m-%d")
+        target_date = target_date.replace(year=datetime.now().year)
+        if time == "now":
+            target_time = datetime.now()
+        else:
+            target_time = datetime.strptime(time, "%H:00") 
+        # target_time = datetime.now().strftime("%Y-%m-%dT%H:00")
+        target_datetime = target_date.strftime("%Y-%m-%d") + "T" + target_time.strftime("%H:00")
+        weather_res = requests.get(weather_url, params=weather_params).json()
+
+        if "hourly" in weather_res:
+            for i, time in enumerate(weather_res["hourly"]["time"]):
+                if time == target_datetime:
+                    break
+
+        temp = round(weather_res["hourly"]["temperature_2m"][i])
+        feels = weather_res["hourly"]["apparent_temperature"][i]
+        weather_code = weather_res["hourly"]["weathercode"][i]
+        wind_speed = weather_res["hourly"]["wind_speed_10m"][i]
+        condition = WMO.get(weather_code, {"name": "Unknown", "emoji": ""})
+
+        maxTemps = {}
+
+        time = target_time.strftime("%H:00");
+
+        if "hourly" in weather_res:
+            for timeStr, temperature in zip(weather_res["hourly"]["time"], weather_res["hourly"]["temperature_2m"]):
+                dateFor = datetime.fromisoformat(timeStr).date()
+
+                if dateFor not in maxTemps or temperature > maxTemps[dateFor]:
+                    maxTemps[dateFor] = temperature
+
+        print(maxTemps)
+
+        return {
+            "temperature": temp,
+            "feeling": feels,
+            "wind": wind_speed,
+            "condition": condition,
+            "time": time
+        }
+    
+    def get_available_cities(self):
+        european_capitals = [
+            "Vienna", "Brussels", "Sofia", "Zagreb", "Nicosia", "Prague", "Copenhagen", 
+            "Tallinn", "Helsinki", "Paris", "Berlin", "Athens", "Budapest", "Reykjavik", 
+            "Dublin", "Rome", "Riga", "Vaduz", "Vilnius", "Luxembourg", "Valletta", 
+            "Chisinau", "Monaco", "Podgorica", "Amsterdam", "Skopje", "Oslo", "Warsaw", 
+            "Lisbon", "Bucharest", "Moscow", "San Marino", "Belgrade", "Bratislava", 
+            "Ljubljana", "Madrid", "Stockholm", "Bern", "Kiev", "London"]
+
+        romanian_cities = [
+            "Bucharest", "Cluj-Napoca", "Timișoara", "Iași", "Constanța", "Craiova", 
+            "Brașov", "Galați", "Ploiești", "Oradea", "Brăila", "Arad", "Pitești", 
+            "Sibiu", "Bacău", "Târgu Mureș", "Baia Mare", "Buzău", "Satu Mare", 
+            "Râmnicu Vâlcea", "Drobeta-Turnu Severin", "Suceava", "Piatra Neamț", 
+            "Târgu Jiu", "Focșani", "Botoșani", "Rădăuți", "Reșița", "Hunedoara"]
+
+        all_cities = european_capitals + romanian_cities
+        all_cities = sorted(list(set(all_cities)))
+        
+        return all_cities
+    
+api = Api()
 
 webview.create_window(
     "Weather App",
     "index.html",
-    width=900,
-    height=720,
+    js_api=api,
+    width=960,
+    height=760,
     resizable=False
 )
 
-webview.start()
+webview.start(debug=True)
